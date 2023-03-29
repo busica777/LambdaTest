f42b88210e6af4cfac1119a1f6677719b7ce28f1
sergiopele <sergiypele@gmail.com>
2023-03-29 19:37:36 -0400

    s

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,7 +2,8 @@ package utils;
 
 public interface Constants {
 	String CONFIGURATION_FILE_PATH = "src/test/resources/config/config.properties";
-	int IMPLICIT_WAIT = 20; int EXPLICIT_WAIT = 21;
+	int IMPLICIT_WAIT = 20;
+	int EXPLICIT_WAIT = 20;
 	String SCREENSHOT_FILEPATH = System.getProperty("user.dir") + "/screenshots/";
 	
 }
