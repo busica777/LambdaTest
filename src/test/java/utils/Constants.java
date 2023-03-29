27797a55e57e240119abb3cebe743f1c0c6543c9
sergiopele <sergiypele@gmail.com>
2023-03-29 19:33:26 -0400

    I added some changes here

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,7 +2,6 @@ package utils;
 
 public interface Constants {
 	String CONFIGURATION_FILE_PATH = "src/test/resources/config/config.properties";
-	//dsfsdfdsfdsfds
 	int IMPLICIT_WAIT = 20; int EXPLICIT_WAIT = 20;
 	String SCREENSHOT_FILEPATH = System.getProperty("user.dir") + "/screenshots/";
 	
